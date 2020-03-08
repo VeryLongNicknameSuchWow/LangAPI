1418e9fc27236200e0cf775a23cdbb8bf5ca826e
BuildTools <szymonwitek02@gmail.com>
2020-03-08 22:07:50 +0100

    Fix

--- src/main/java/pl/rynbou/langapi/LangMassage.java
+++ src/main/java/pl/rynbou/langapi/LangMassage.java
@@ -24,12 +24,12 @@ public class LangMassage {
 
     public LangMassage(ConfigurationSection section) {
 
-        useChat = section.getBoolean("use-chat");
+        useChat = section.getBoolean("chat-enabled");
         if (useChat) {
             chatContent = color(section.getString("chat.content"));
         }
 
-        useTitle = section.getBoolean("use-title");
+        useTitle = section.getBoolean("title-enabled");
         if (useTitle) {
             titleContent = color(section.getString("title.content"));
             subtitleContent = color(section.getString("title.sub-content"));
@@ -38,7 +38,7 @@ public class LangMassage {
             fadeOut = section.getInt("title.fade-out");
         }
 
-        useActionBar = section.getBoolean("use-actionbar");
+        useActionBar = section.getBoolean("actionbar-enabled");
         if (useActionBar) {
             actionBarContent = color(section.getString("actionbar.content"));
         }
